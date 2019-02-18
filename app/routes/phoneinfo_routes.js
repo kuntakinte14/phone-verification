08cbfe4b860d92c9cc1a109ee10f8b1218442e53
Edward Callender <tcallend@ix.netcom.com>
2019-02-18 10:34:49 -0800

    added sms gateway http callback handler

--- app/routes/phoneinfo_routes.js
+++ app/routes/phoneinfo_routes.js
@@ -1,11 +1,95 @@
 // routes/phoneinfo_routes.js
 var moment = require('moment');
+var qs = require('querystring');
 var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(app, db) {
 	const collection = 
+
+	// this is just for testing, the post handler is what is being used moving 
+	// forward
+	app.get('/sms-gateway', (req, res) => {
+		console.log("sms gateway http get callback:");
+		//console.log(req);
+		var str = req.url.split('?')[1];
+		var urlParams = qs.parse(str);
+		//console.log(urlParams);
+		
+		//console.log(urlParams.message);
+		messageParams = urlParams.message.split(" ");
+		//console.log(messageParams);
+		var code;
+		for (i=0; i<=messageParams.length; i++) { 
+			//console.log(parseInt(messageParams[i])); 
+			//curValue = parseInt(messageParams[i]);
+			if (isNaN(messageParams[i])) {
+				console.log("this is text, move on");
+			}
+			else {
+				if (messageParams[i] != "365") {
+				    code = messageParams[i];
+				    console.log("code is: "+code);
+				}
+			}
+		} 
+		
+		//res.send("hello!");
+		res.send(urlParams);
+	});
 	
-	// insert a row	
+	// This processes the http callback sent from the diafaan SMS gateway when
+	// an incoming message is received by that server
+	app.post('/sms-gateway', (req, res) => {
+		//console.log("sms gateway http post callback:");
+		//console.log(req.body);
+		
+		messageParams = req.body.message.split(" ");
+		//console.log(messageParams);
+		var code;
+		for (i=0; i<=messageParams.length; i++) { 
+			//console.log(parseInt(messageParams[i])); 
+			//curValue = parseInt(messageParams[i]);
+			if (isNaN(messageParams[i])) {
+				console.log("this is text, move on");
+			}
+			else {
+				if (messageParams[i] != "365") {
+				    code = messageParams[i];
+				    console.log("code is: "+code);
+				}
+			}
+		} 		
+		
+		const phone = { 
+				host_number: req.body.to, 
+				remote_number: req.body.from, 
+				message: code, 
+				full_message_body: req.body,
+				created_on_timestamp: moment.utc().format(), 
+				created_on_date: moment.utc().format("MM-DD-YYYY"),
+				first_name: '', 
+				last_name: '', 
+				email_address: '', 
+				phone_number: '',
+				company_name: '',
+				user_id: '',
+				domain_name: '',
+				password: '',
+				challenge_code: '',
+				updated_on_timestamp: ''			
+		};
+		db.collection('phoneinfo').insert(phone, (err, result) => {
+		  if (err) { 
+		    res.send({ 'error': 'An error has occurred' }); 
+		  } else {
+		    res.send(result.ops[0]);
+		  }
+		});		
+		
+		//res.send("http post callback received and processed!");
+	});	
+		
+	// insert a row, with host_number, remote_number, and message values passed in the http post call
 	app.post('/phoneinfo', (req, res) => {
 		console.log("phoneinfo sent: "+ req.body.host_number + " + "+ req.body.message);
 		const phone = { 
