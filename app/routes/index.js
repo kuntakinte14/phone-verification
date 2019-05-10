3d2f8ed5d8fabf3fcd9bba560e60d0324d169d8d
Edward Callender <tcallend@ix.netcom.com>
2019-05-10 07:56:16 -0700

    added getsmscode route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,10 @@
 // routes/index.js
 
 const phoneInfoRoutes = require('./phoneinfo_routes');
+const getSMSRoutes = require('./getsms_routes');
 
 module.exports = function(app, db) {
   phoneInfoRoutes(app, db);
+  getSMSRoutes(app, db);
   // Other route groups could go here, in the future
 };
