2b04d9db72cd45d6ce0f2217a26abb2fbb1f1eb6
Edward Callender <tcallend@ix.netcom.com>
2019-05-27 11:37:17 -0700

    latest modification

--- app/routes/index.js
+++ app/routes/index.js
@@ -15,7 +15,8 @@ module.exports = function(app, db) {
   
   app.get('/dashboard', (req,res) => {
 	  //res.send('this is the getsms page');
-      db.collection('phoneinfo').find({ 'pid':'0'}).sort({_id:-1}).toArray( function(err, results) {
+	  var phoneNumbers = ['2066362673','2066362672','6692042121','6285027003','2062357593','2062359088'];
+      db.collection('phoneinfo').find({'pid':'0','receiver':{$in:phoneNumbers}}).sort({_id:-1}).toArray( function(err, results) {
           if (err) {
             res.send({'error':'An error has occurred'});
           } else {
